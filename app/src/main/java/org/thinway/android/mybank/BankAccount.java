9b31cb4c1150f71d02e1c450b04a3d297677874b
Fran Thinway <thinway@icloud.com>
2015-01-26 18:04:23 +0100

    Added balance field to BankAccount and its getter.

--- app/src/main/java/org/thinway/android/mybank/BankAccount.java
+++ app/src/main/java/org/thinway/android/mybank/BankAccount.java
@@ -4,11 +4,17 @@ package org.thinway.android.mybank;
  * Created by thinway on 26/1/15.
  */
 public class BankAccount {
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
     public void withdraw(double amount){
 
     }
 
     public void deposit(double amount){
-        
+
     }
 }
