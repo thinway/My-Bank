fa14633812f40443a06d6c0bf3e91c77167de284
Fran Thinway <thinway@icloud.com>
2015-01-26 18:01:01 +0100

    Added BankAccount class.

---
+++ app/src/main/java/org/thinway/android/mybank/BankAccount.java
@@ -0,0 +1,14 @@
+package org.thinway.android.mybank;
+
+/**
+ * Created by thinway on 26/1/15.
+ */
+public class BankAccount {
+    public void withdraw(double amount){
+
+    }
+
+    public void deposit(double amount){
+        
+    }
+}
